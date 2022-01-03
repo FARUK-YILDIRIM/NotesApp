bf014041f275221d3ecf2910ba3ae0e500e800ad
Faruk YILDIRIM <faruk@softalika.com>
2022-01-03 20:14:53 +0300

    project setup with firebase

--- NotesApp/NotesAppApp.swift
+++ NotesApp/NotesAppApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct NotesAppApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
