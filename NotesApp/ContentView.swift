4c694757cd6b0e5b041686bc6e33e1ab789d7667
Faruk YILDIRIM <faruk@softalika.com>
2022-01-03 20:01:41 +0300

    Initial Commit

---
+++ NotesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp
+//
+//  Created by Faruk YILDIRIM on 3.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Faruk YILDIRIM on 3.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
